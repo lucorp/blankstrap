e2b756bb36b76791a16df793814f12f51f6e6b3d
Štefan Lučivjanský <stefan.lucivjansky@gmail.com>
2018-03-02 12:56:38 +0100

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-xs-12 col-md-12 col-xl-12 col-lg-12">
+            <h1>404: Nenájdené!</h1>
+            <p>Požadovaná stránka sa u nás nenašla.</p>
+            <p>Ste si istí, že ste zadali správnu adresu?</p>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
